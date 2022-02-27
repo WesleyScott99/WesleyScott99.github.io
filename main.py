1f5498ad09223adff01180d3f84e6911faca0055
WesleyScott99 <86537246+WesleyScott99@users.noreply.github.com>
2022-02-26 21:19:13 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
-print("hello world")
+from flask import Flask, render_template, flash
+from content_management import Content
+
+...
+
+@app.route('/dashboard/')
+def dashboard():
+    flash("flash test!!!!")
+    flash("fladfasdfsaassh test!!!!")
+    flash("asdfas asfsafs!!!!")
+    return render_template("dashboard.html", TOPIC_DICT = TOPIC_DICT)
